11f0f59f0d6cc259d7566941e76472ae8913bb03
Oleksandr Vinnichuk <oleksandr.v@quintagroup.org>
2016-12-06 15:27:13 +0200

    Add sha hashing for tender token

--- openprocurement/contracting/api/tests/base.py
+++ openprocurement/contracting/api/tests/base.py
@@ -5,12 +5,13 @@ import webtest
 from uuid import uuid4
 from copy import deepcopy
 from datetime import datetime
+from hashlib import sha512
 
 from openprocurement.api.utils import VERSION
 
 
 now = datetime.now()
-
+test_tender_token = uuid4().hex
 test_contract_data = {
     u"items": [
         {
@@ -102,7 +103,7 @@ test_contract_data = {
     u"id": uuid4().hex,
     u"contractID": u"UA-2016-03-18-000001-1",
     u"tender_id": uuid4().hex,
-    u"tender_token": uuid4().hex,
+    u"tender_token": sha512(test_tender_token).hexdigest(),
     u"owner": u"broker"
 }
 
