8fafbaaf489ea5d76a8caa318943f35485427101
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-04-07 11:33:34 +0300

    Added items deliveryLocation validation

--- openprocurement/contracting/api/models.py
+++ openprocurement/contracting/api/models.py
@@ -10,6 +10,7 @@ from schematics.types.serializable import serializable
 from schematics.exceptions import ValidationError
 from schematics.transforms import whitelist, blacklist
 from openprocurement.api.models import get_now
+from openprocurement.api.models import Location as BaseLocation
 from openprocurement.api.models import Contract as BaseContract
 from openprocurement.api.models import Document as BaseDocument
 from openprocurement.api.models import Organization as BaseOrganization
@@ -99,7 +100,23 @@ class ProcuringEntity(Organization):
     kind = StringType(choices=['general', 'special', 'defense', 'other'])
 
 
+class Location(BaseLocation):
+
+    def validate_latitude(self, data, latitude):
+        """ NOTE: Disable validation on model levev and move it to view level.
+              Related to time difference between tender 
+                             and contract creation moment
+        """
+        pass
+
+    def validate_longitude(self, data, longitude):
+        """same as validatate_latitude"""
+        pass
+
+
 class Item(BaseItem):
+
+    deliveryLocation = ModelType(Location)
 
     class Options:
         roles = {
--- openprocurement/contracting/api/validation.py
+++ openprocurement/contracting/api/validation.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.utils import update_logging_context
+from schematics.exceptions import ValidationError
+
+from openprocurement.api.utils import update_logging_context, error_handler
 from openprocurement.api.validation import validate_json_data, validate_data
 from openprocurement.contracting.api.models import Contract, Change
+from openprocurement.api.models import validate_coordinate
+
+
+def validate_items_delivery_location(request):
+    """
+    NOTE: validation for items delivery locaion is disabled on models level.
+    Related to time difference:
+        {{ tender}} <- {{ validation activation }} <- {{ contract}}
+             ^---------< one contracting process >---------^
+    see: https://app.asana.com/0/153268966093617/200894405797627
+    """
+    items = request.validated['data'].get('items', [])
+    if not items:
+        return
+    for item in items:
+        if item.get('deliveryLocation'):
+            longitude = item.get('deliveryLocation', {}).get('longitude')
+            latitude = item.get('deliveryLocation', {}).get('latitude')
+            orig_item = reduce(next, [
+                i for i in getattr(request.context, 'items', []) if i.id == item['id']
+            ], iter(()))
+            if not orig_item:
+                if latitude:
+                    validate_coordinate(-90, 90, 'latitude')(latitude)
+                if longitude:
+                    validate_coordinate(-180, 180, 'longitude')(longitude)
+            elif orig_item and hasattr(orig_item, 'deliveryLocation'):
+                if latitude and latitude != orig_item.deliveryLocation.latitude:
+                    validate_coordinate(-90, 90, 'latitude')(latitude)
+                if longitude and longitude != orig_item.deliveryLocation.longitude:
+                    validate_coordinate(-180, 180, 'longitude')(longitude)
 
 
 def validate_contract_data(request):
