11c5ca7a0148caa7edbec74879e73c40a174d9dd
annawzz <annawzz@quintagroup.org>
2017-05-24 15:44:51 +0300

    Update include me

--- openprocurement/contracting/api/__init__.py
+++ openprocurement/contracting/api/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from logging import getLogger
 from pkg_resources import get_distribution
-from openprocurement.contracting.api.models import CommonContract, ESCOContract
-from openprocurement.contracting.api.utils import contract_from_data, extract_contract, register_contract_contractType
-from openprocurement.contracting.api.design import add_design
 
 PKG = get_distribution(__package__)
 
@@ -12,6 +9,9 @@ LOGGER = getLogger(PKG.project_name)
 
 def includeme(config):
     LOGGER.info('Init contracting plugin.')
+    from openprocurement.contracting.api.models import CommonContract
+    from openprocurement.contracting.api.utils import contract_from_data, extract_contract, register_contract_contractType
+    from openprocurement.contracting.api.design import add_design
     add_design()
     config.registry.contract_contractTypes = {}
     config.add_directive('add_contract_contractType',
@@ -24,8 +24,7 @@ def includeme(config):
 
 def includeme_esco(config):
     LOGGER.info('Init esco contracting plugin.')
-    add_design()
+    from openprocurement.contracting.api.models import ESCOContract
     config.add_contract_contractType(ESCOContract)
-    config.add_request_method(extract_contract, 'contract', reify=True)
-    config.add_request_method(contract_from_data)
-    config.scan("openprocurement.contracting.api.views")
+    # Should be uncommented when esco contracts will be moved to own package
+    # config.scan("openprocurement.contracting.api.views")
