03cf7583307865f5587aba8301515417a96bd9e9
annawzz <annawzz@quintagroup.org>
2017-05-24 12:12:38 +0300

    Add plugins for esco contracting

--- openprocurement/contracting/api/__init__.py
+++ openprocurement/contracting/api/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from logging import getLogger
 from pkg_resources import get_distribution
+from openprocurement.contracting.api.models import CommonContract, ESCOContract
+from openprocurement.contracting.api.utils import contract_from_data, extract_contract, register_contract_contractType
+from openprocurement.contracting.api.design import add_design
 
 PKG = get_distribution(__package__)
 
@@ -8,10 +11,21 @@ LOGGER = getLogger(PKG.project_name)
 
 
 def includeme(config):
-    from openprocurement.contracting.api.utils import contract_from_data, extract_contract
-    from openprocurement.contracting.api.design import add_design
     LOGGER.info('Init contracting plugin.')
     add_design()
+    config.registry.contract_contractTypes = {}
+    config.add_directive('add_contract_contractType',
+                         register_contract_contractType)
+    config.add_contract_contractType(CommonContract)
+    config.add_request_method(extract_contract, 'contract', reify=True)
+    config.add_request_method(contract_from_data)
+    config.scan("openprocurement.contracting.api.views")
+
+
+def includeme_esco(config):
+    LOGGER.info('Init esco contracting plugin.')
+    add_design()
+    config.add_contract_contractType(ESCOContract)
     config.add_request_method(extract_contract, 'contract', reify=True)
     config.add_request_method(contract_from_data)
     config.scan("openprocurement.contracting.api.views")
