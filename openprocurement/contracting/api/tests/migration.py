998a2b02f63d965ba38f2181f4a4273e2a4bdee5
Taras Vaskiv <codertarasvaskiv@quintagroup.org>
2018-03-26 11:58:18 +0300

    Fix test migration

--- openprocurement/contracting/api/tests/migration.py
+++ openprocurement/contracting/api/tests/migration.py
@@ -27,40 +27,38 @@ class MigrateTest(BaseWebTest):
         migrate_data(self.app.app.registry, 1)
         self.assertEqual(get_db_schema_version(self.db), SCHEMA_VERSION)
 
-#    def test_migrate_from0to1(self):
-#        set_db_schema_version(self.db, 0)
-#
-#        with open(os.path.join(os.path.dirname(__file__), 'data/tender-contract-complete.json'), 'r') as df:
-#            data = json.loads(df.read())
-#
-#        t = Tender(data)
-#        t.store(self.db)
-#        tender = self.db.get(t.id)
-#
-#        self.assertEqual(tender['awards'][0]['value'], data['awards'][0]['value'])
-#        self.assertEqual(tender['awards'][0]['suppliers'], data['awards'][0]['suppliers'])
-#
-#        contract_data = deepcopy(tender['contracts'][0])
-#        del contract_data['value']
-#        del contract_data['suppliers']
-#        contract_data['tender_id'] = tender['_id']
-#        contract_data['tender_token'] = 'xxx'
-#        contract_data['procuringEntity'] = tender['procuringEntity']
-#
-#        contract = Contract(contract_data)
-#        contract.dateModified = get_now()
-#        contract.store(self.db)
-#        contract_data = self.db.get(contract.id)
-#        self.assertNotIn("value", contract_data)
-#        self.assertNotIn("suppliers", contract_data)
-#
-#        migrate_data(self.app.app.registry, 1)
-#        migrated_item = self.db.get(contract.id)
-#
-#        self.assertIn("value", migrated_item)
-#        self.assertEqual(migrated_item['value'], tender['awards'][0]['value'])
-#        self.assertIn("suppliers", migrated_item)
-#        self.assertEqual(migrated_item['suppliers'], tender['awards'][0]['suppliers'])
+    def test_migrate_from0to1(self):
+        set_db_schema_version(self.db, 0)
+
+        with open(os.path.join(os.path.dirname(__file__), 'data/tender-contract-complete.json'), 'r') as df:
+            data = json.loads(df.read())
+        t = Tender(data)
+        t.store(self.db)
+        tender = self.db.get(t.id)
+        self.assertEqual(tender['awards'][0]['value'], data['awards'][0]['value'])
+        self.assertEqual(tender['awards'][0]['suppliers'], data['awards'][0]['suppliers'])
+
+        contract_data = deepcopy(tender['contracts'][0])
+        del contract_data['value']
+        del contract_data['suppliers']
+        contract_data['tender_id'] = tender['_id']
+        contract_data['tender_token'] = 'xxx'
+        contract_data['procuringEntity'] = tender['procuringEntity']
+        contract = Contract(contract_data)
+        contract.dateModified = get_now()
+        contract.store(self.db)
+        contract_data = self.db.get(contract.id)
+        self.assertNotIn("value", contract_data)
+        self.assertNotIn("suppliers", contract_data)
+
+        migrate_data(self.app.app.registry, 1)
+        migrated_item = self.db.get(contract.id)
+        self.assertIn("value", migrated_item)
+        tender['awards'][0]['value']['amount'] = \
+            str(tender['awards'][0]['value']['amount'])
+        self.assertEqual(migrated_item['value'], tender['awards'][0]['value'])
+        self.assertIn("suppliers", migrated_item)
+        self.assertEqual(migrated_item['suppliers'], tender['awards'][0]['suppliers'])
 
     def test_migrate_from1to2(self):
         set_db_schema_version(self.db, 1)
