1bf16d4a5d7c84d0073949aa12955343cbdfcff2
annawzz <annawzz@quintagroup.org>
2018-04-02 09:52:03 +0300

    Rewrite contract_from_data, use adapters

--- openprocurement/contracting/api/utils.py
+++ openprocurement/contracting/api/utils.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from functools import partial
-from pkg_resources import get_distribution
-from logging import getLogger
+from pyramid.compat import decode_path_info
+from pyramid.exceptions import URLDecodeError
 from cornice.resource import resource
-from openprocurement.api.utils import (
-    error_handler,
-)
 
+from openprocurement.api.utils import error_handler
 from openprocurement.contracting.api.traversal import factory
-from openprocurement.contracting.core.models import Contract
 
 
 contractingresource = partial(
@@ -17,13 +14,8 @@ contractingresource = partial(
     factory=factory
 )
 
-PKG = get_distribution(__package__)
-LOGGER = getLogger(PKG.project_name)
-
-
-def extract_contract(request):
+def extract_contract_adapter(request, contract_id):
     db = request.registry.db
-    contract_id = request.matchdict['contract_id']
     doc = db.get(contract_id)
     if doc is not None and doc.get('doc_type') == 'contract':
         request.errors.add('url', 'contract_id', 'Archived')
@@ -37,7 +29,32 @@ def extract_contract(request):
     return request.contract_from_data(doc)
 
 
+def extract_contract(request):
+    try:
+        # empty if mounted under a path in mod_wsgi, for example
+        path = decode_path_info(request.environ['PATH_INFO'] or '/')
+    except KeyError:
+        path = '/'
+    except UnicodeDecodeError as e:
+        raise URLDecodeError(e.encoding, e.object, e.start, e.end, e.reason)
+
+    contract_id = ""
+    # extract contract id
+    parts = path.split('/')
+    if len(parts) < 4 or parts[3] != 'contracts':
+        return
+
+    contract_id = parts[4]
+    return extract_contract_adapter(request, contract_id)
+
+
 def contract_from_data(request, data, raise_error=True, create=True):
-    if create:
-        return Contract(data)
-    return Contract
+    contractType = data.get('contractType', 'common')
+    model = request.registry.contract_contractTypes.get(contractType)
+    if model is None and raise_error:
+        request.errors.add('data', 'contractType', 'Not implemented')
+        request.errors.status = 415
+        raise error_handler(request.errors)
+    if model is not None and create:
+        model = model(data)
+    return model
